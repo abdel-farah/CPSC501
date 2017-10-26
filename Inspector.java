4650ad0f23d2a48c77c13a7e18d1c28d90f91e3e
abdel-farah <abdel.farah@ucalgary.ca>
2017-10-26 14:29:09 -0600

    Second commit
    Implemented all basic observations, still need to do recursion

--- Inspector.java
+++ Inspector.java
@@ -6,12 +6,41 @@ public class Inspector{
 
 	public Inspector(){}
 	public void inspect(Object obj, boolean recursive){
+	   
 	    Class classObject = obj.getClass();
+		Class superClassObject = classObject.getSuperclass();
 	    Method[] methodArray = classObject.getDeclaredMethods();
 	    Field[] fieldArray = classObject.getDeclaredFields();
-     
+		Class[] interArray = classObject.getInterfaces();
+		Constructor[] constArray = classObject.getConstructors();
+		
 	    System.out.println("inside inspector: " + obj + " (recursive = "+recursive+")");
-	  
+		
+		
+		
+		if (superClassObject != null ){
+			System.out.println("super class: " + superClassObject.getName());
+		}
+	   
+	   for ( int i = 0; i<interArray.length; i++){
+			System.out.println("interface(s): " + interArray[i].getName());
+	   }
+	   
+	   
+	    for ( int i = 0; i<constArray.length; i++){
+			Class[] paramArray = constArray[i].getParameterTypes();
+			int modifier = constArray[i].getModifiers();
+			String name = constArray[i].getName();
+			System.out.print("Constructor: " + name + " modifier: " + modifier);
+
+			for ( int h =0; h < paramArray.length; h++){
+				System.out.print(" Parameters: " + paramArray[h].getName());
+	   }
+   
+			
+			System.out.println();
+   }
+	   
 	    for ( int i =0; i< methodArray.length; i++){
 		String name = methodArray[i].getName();
 		Class type = methodArray[i].getReturnType();
@@ -30,6 +59,10 @@ public class Inspector{
 			}
 			System.out.println();
 		}
+	    
+	    
+	    
+	    
 	    
 	    for (int i = 0; i<  fieldArray.length; i++){
 		//String name = fieldArray[i].getName();
